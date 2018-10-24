42886a9f83b5953273be82a453501d1ca6e32491
Sdeyoung22 <43021130+Sdeyoung22@users.noreply.github.com>
2018-10-24 12:44:11 -0400

    Add files via upload

---
+++ src/StringManipulator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Sdeyoung22
+ *
+ */
+public class StringManipulator
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner user = new Scanner(System.in);
+		System.out.print("Enter any string:  ");
+		String str = user.nextLine();
+		int strLength = str.length();
+		System.out.print("Enter the character you would like to search for:  ");
+		String search = user.nextLine();
+		
+		
+		
+		
+
+	}
+
+}
